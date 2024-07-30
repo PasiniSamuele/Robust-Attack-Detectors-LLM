28d301d06f777e2a3dad4939a58f18d7d886d75d
PasiniSamuele <samu.pasini98@gmail.com>
2024-07-30 17:30:20 +0200

    removed scripts

--- src/write_test_results_xss.sh
+++
@@ -1,8 +0,0 @@
-> test_results_xss.txt
-
-# Read each line from runs.txt
-while IFS= read -r run
-do
-  # Find all "test_results.csv" files in the directory and its subdirectories
-  find "$run" -type f -name "test_results.csv" -printf "%p\n" >> test_results_xss.txt
-done < runs_xss.txt
